4fd2f309972c4107668027ca9c30ca179d00b49e
sjohnnlee <killerlee1992@gmail.com>
2021-04-09 13:36:54 -0700

    fixed eddit page

--- src/components/edit-cage.components.js
+++ src/components/edit-cage.components.js
@@ -6,6 +6,12 @@ export default class EditCage extends Component {
     constructor(props) {
         super(props);
 
+        this.onChangeCageName = this.onChangeCageName.bind(this)
+        this.onChangeCageHens = this.onChangeCageHens.bind(this)
+        this.onChangeCageRoos = this.onChangeCageRoos.bind(this)
+        this.onChangeCageTotal = this.onChangeCageTotal.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+
         this.state = {
             cage_name:'',
             cage_hen:"",
@@ -13,6 +19,9 @@ export default class EditCage extends Component {
             cage_total:"",
         }
    }
+   
+        
+
         componentDidMount(){
         axios.get('http://localhost:4000/cages/'+this.props.match.params.id)
             .then(response => {
@@ -26,6 +35,44 @@ export default class EditCage extends Component {
             .catch(function(error){
                 console.log(error)
             })
+        }
+
+        onChangeCageName(e){
+            this.setState({
+                cage_name:e.target.value
+            })
+        }
+        onChangeCageHens(e){
+            this.setState({
+                cage_hen: e.target.value
+            })
+        }
+        onChangeCageRoos(e){
+            this.setState({
+                cage_roo: e.target.value
+            })
+        }
+        onChangeCageTotal(e){
+            this.setState({
+                cage_total: e.target.value
+            })
+            }
+
+        onSubmit(e) {
+            e.preventDefault();
+            const obj= {
+                cage_name:this.state.cage_name,
+                cage_hen:this.state.cage_hen,
+                cage_roo:this.state.cage_roo,
+                cage_total:this.state.cage_total,
+            };
+            axios.post('http://localhost:4000/cages/update/'+this.props.match.params.id, obj)
+            .then(res => console.log(res.data)
+            ).catch(err => {
+                console.log("onSubmit newCage error", err)
+            });
+
+            this.props.history.push('/')
         }
     
     
@@ -42,8 +89,33 @@ export default class EditCage extends Component {
                                 onChange={this.onChangeCageName}
                                 />
                     </div>
-
+                    <div className="form-group">
+                        <label>Hen</label>
+                        <input type="number"
+                                className="form-control"
+                                value={this.state.cage_hen}
+                                onChange={this.onChangeCageHens}
+                                />
+                    </div>
+                    <div className="form-group">
+                        <label>Roos</label>
+                        <input type="number"
+                                className="form-control"
+                                value={this.state.cage_roo}
+                                onChange={this.onChangeCageRoos}
+                                />
+                    </div>
+                    <div className="form-group">
+                        <label>Total</label>
+                        <input type="number"
+                                className="form-control"
+                                value={this.state.cage_total = (+this.state.cage_hen )+ (+this.state.cage_roo)}
+                                onChange={this.onChangeCageTotal}
+                                />
+                    </div>
+                         <input type="submit" value="Submit Cage" className="btn btn-primary"/>
                 </form>
+               
             </div>
         )
     }
